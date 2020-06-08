03717fa76cc12e07c2fa4ee6a4eaf8301d6832cf
Ali Badran <ali.h.badran@hotmail.com>
2020-06-08 17:40:10 +0300

    First Commit

---
+++ DiscussionForumMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiscussionForumMVC.Startup))]
+namespace DiscussionForumMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
